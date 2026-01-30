3e731a6d509eeb0b82a75f04541b41be7fdde924
zhaoshuailei <zhaoshuailei@paxsz.com>
2026-01-30 13:37:32 +0800

    Init Project

---
+++ demo/src/main/java/com/pax/android/demoapp/base/BaseApplication.java
@@ -0,0 +1,59 @@
+package com.pax.android.demoapp.base;
+
+import android.app.Application;
+import android.graphics.BitmapFactory;
+import android.os.RemoteException;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.pax.android.demoapp.R;
+import com.pax.market.android.app.sdk.BaseApiService;
+import com.pax.market.android.app.sdk.StoreSdk;
+import com.pax.market.android.app.sdk.msg.utils.Notifications;
+
+
+/**
+ * Created by fojut on 2017/8/24.
+ */
+
+public class BaseApplication extends Application {
+    private static final String TAG = BaseApplication.class.getSimpleName();
+
+    //todo make sure to replace with your own app's appkey and appsecret
+    private static final String appkey = "DQOSMMGSBDCNIXOFZR1U";
+    private static final String appSecret = "PMKNAMTBLBGZ2ARS110YQ7LCGT7UCU0NPO74ESS4";
+
+
+    //todo please make sure get the correct SN here, for pax device you can integrate NeptuneLite SDK to get the correct SN
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //initial the SDK
+        initStoreSdk();
+    }
+
+    private void initStoreSdk() {
+        //todo 1. Init AppKey，AppSecret and SN, make sure the appkey and appSecret is corret.
+        StoreSdk.getInstance().init(getApplicationContext(), appkey, appSecret, new BaseApiService.Callback() {
+            @Override
+            public void initSuccess() {
+                Log.i(TAG, "initSuccess.");
+
+            }
+
+            @Override
+            public void initFailed(RemoteException e) {
+                Log.i(TAG, "initFailed: "+e.getMessage());
+                Toast.makeText(getApplicationContext(), "Cannot get API URL from STORE client," +
+                        " Please install STORE client first.", Toast.LENGTH_LONG).show();
+            }
+        });
+        //if you want to customize the notification for Cloud Message.
+        Notifications.I.init(getApplicationContext())
+                .setSmallIcon(R.drawable.logo_demo_white)
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.logo_demo));
+
+        // disable the Notifications we provided through below code.
+        // Notifications.I.setEnabled(false);
+    }
+}
---
+++ demo/src/main/java/com/pax/android/demoapp/utils/SPUtil.java
@@ -0,0 +1,74 @@
+package com.pax.android.demoapp.utils;
+
+import android.content.Context;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.pax.market.android.app.sdk.util.PreferencesUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//to store result cross process, replace shared preferences.
+public class SPUtil {
+
+
+    public void setString(Context context, String tag, String value) {
+       PreferencesUtils.putString(context, tag, value);
+    }
+
+    public String getString(Context context, String tag) {
+        String value = PreferencesUtils.getString(context, tag,null);
+        return value;
+
+    }
+
+    /**
+     * save List
+     * @param tag
+     * @param datalist
+     */
+    public <T> void setDataList(Context context, String tag, List<T> datalist) {
+        if (null == datalist || datalist.size() <= 0)
+            return;
+        Gson gson = new Gson();
+        //转换成json数据，再保存
+        String strJson = gson.toJson(datalist);
+        PreferencesUtils.putString(context, tag, strJson);
+
+    }
+
+    /**
+     * get List
+     * @param tag
+     * @return
+     */
+    public <T> List<T> getDataList(Context context, String tag) {
+        List<T> datalist=new ArrayList<T>();
+        String strJson = PreferencesUtils.getString(context, tag, null);
+        if (null == strJson) {
+            return datalist;
+        }
+        Gson gson = new Gson();
+        datalist = gson.fromJson(strJson, new TypeToken<List<T>>() {
+        }.getType());
+        return datalist;
+
+    }
+
+    /**
+     * get List
+     *
+     * @param tag
+     * @return
+     */
+    public <T> T getDataListByType(Context context, String tag, TypeToken<T> typeToken) {
+        String strJson = PreferencesUtils.getString(context, tag, null);
+        if (null == strJson) {
+            return null;
+        }
+        Gson gson = new Gson();
+        return gson.fromJson(strJson, typeToken.getType());
+
+    }
+}
